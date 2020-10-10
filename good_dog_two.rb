8761b2e66bd08500c91ead0c9f219d476eb7951f
Thai Do <thai.hl.do@gmail.com>
2020-10-10 15:52:42 -0700

    Created getter and setter methods

--- good_dog_two.rb
+++ good_dog_two.rb
@@ -2,6 +2,20 @@ class GoodDog
   def initialize(name)
     @name = name
   end
+
+  # getting method
+  def get_name
+    @name
+  end
+
+  # setter method
+  def set_name=(name)
+    @name = name
+  end
+
+  def speak
+    "#{@name} says Arf!"
+  end
 end
 
 # The string "Sparky" is being passed from the new method through
@@ -10,7 +24,15 @@ end
 # instance variable @name to name, which results in assigning the string
 # "Sparky" to the @name istance variable
 sparky = GoodDog.new("Sparky") 
+puts sparky.speak
 
+fido = GoodDog.new("Fido")
+puts fido.speak
 
+# Original name was sparky
+puts sparky.get_name
 
-
+# using the setting method we can change the name
+sparky.set_name = "Spartacus"
+# New name should be Spartacus
+puts sparky.get_name
